bf1ad1c03f7907ac4f03240eb0fea19dbbc90dda
Meeka <rrgayhart@gmail.com>
2014-01-07 13:58:28 -0700

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Apolloio::Application.config.secret_key_base = '68166539391e8397e81a7a13a1b768a5e2246538b2e602849af605014dfbbdbed09c01888f5d78cdbda23e79d269bf06ef282aea82f4a0fa044bda263229459b'
