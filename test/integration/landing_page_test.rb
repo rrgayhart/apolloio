876d297683c56e02323630e8905c672a6df9e92b
Jonah <Jonahkmoses@gmail.com>
2014-01-08 14:03:23 -0700

    add styling adjustment to index and navbar

--- test/integration/landing_page_test.rb
+++ test/integration/landing_page_test.rb
@@ -6,8 +6,14 @@ class NonUserNavbarTest < Capybara::Rails::TestCase
   	visit '/'
   	assert page.has_content?("Login")
   	assert page.has_css?("#navbar-logo")
-  	refute page.has_content?("Logout")
+  	refute page.has_content?("Log Out")
   end
+
+  # test "logged in user navbar" do
+  #   visit '/'
+    
+  #   assert page.has_content("Log Out")
+  # end
 
 
 
