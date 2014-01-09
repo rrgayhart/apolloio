f3969cb23d2f7994e5ac2445d94d4983d2a2c1b3
QuillyT <QuillyT@gmail.com>
2014-01-09 09:37:44 -0700

    Added api_account_id to goal migration. Setup relationship between goal/api_account in models

--- app/models/api_account.rb
+++ app/models/api_account.rb
@@ -1,4 +1,5 @@
 class ApiAccount < ActiveRecord::Base
   belongs_to :user
   belongs_to :api
+  has_many :goals
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,4 @@
 class Goal < ActiveRecord::Base
   belongs_to :user
+  belongs_to :api_account
 end
