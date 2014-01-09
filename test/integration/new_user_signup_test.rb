e72b88765c3e45ded4dbfabf56c530b7b9cc0a35
QuillyT <QuillyT@gmail.com>
2014-01-09 13:48:46 -0700

    Add test messages

--- test/integration/new_user_signup_test.rb
+++ test/integration/new_user_signup_test.rb
@@ -8,9 +8,7 @@ class NewUserSignupTest < Capybara::Rails::TestCase
 
   test "user_signsup_for_app_and_is_redirected_to_twitter" do
     visit root_path
-
     assert_equal current_path, root_path
-
     click_link "Log In"
 
   end
