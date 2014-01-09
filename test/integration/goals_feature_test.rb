a6fbce8119c8e6f5933730a096c61027944d69c5
Jonah <Jonahkmoses@gmail.com>
2014-01-09 15:31:32 -0700

    adjusted styling of goals/reminders

---
+++ test/integration/goals_feature_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class GoalsFeatureTest < Capybara::Rails::TestCase
+
+  setup do
+    visit root_path
+    @auth = OmniAuth.config.mock_auth[:twitter]
+    @user = User.from_omniauth(@auth)
+    click_link "Log In"
+  end
+
+  test "typical goal thing" do
+
+    assert page.has_content?("Add Goal")
+    click_link "Add Goal"
+    assert page.has_content?("Create New Goal")
+    fill_in "Goal Name", :with => "Goal #1"
+    fill_in "Goal Description", :with => "Goal #1"
+    assert_select('select#goal_target option[1]').first['value']
+    # fill_in "Goal Target", :with => "Goal #1"
+    assert_select('select#goal_target option[1]').first['value']
+    fill_in "Goal Period", :with => "Goal #1"
+    fill_in "Goal PeriodType", :with => "Goal #1"
+    fill_in "Goal StartDate", :with => "Goal #1"
+    fill_in "Goal StartDate", :with => "Goal #1"
+  end
+
+end
