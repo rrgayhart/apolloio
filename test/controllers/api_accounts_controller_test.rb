cdc962f0050178cd1b166766a6c18492091da859
QuillyT <QuillyT@gmail.com>
2014-01-09 21:18:37 -0700

    Api Account controller with tests. Started the Show Page view.

---
+++ test/controllers/api_accounts_controller_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class ApiAccountsControllerTest < ActionController::TestCase
+  setup do
+    @api_account = FactoryGirl.create(:api_account)
+  end
+
+  test "There is a show page." do
+    skip
+    get(:show, 'id'=> @api_account.id)
+  end
+end
--- test/integration/api_account_feature_test.rb
+++ test/integration/api_account_feature_test.rb
@@ -8,15 +8,17 @@ class UserLoginTest < Capybara::Rails::TestCase
     goal = Goal.create(user_id: @user.id, name:"Goal")
     @api1 = Api.create(provider: 'Github')
     @api_account = FactoryGirl.create(:api_account, user: @user, api: @api1)
+
+    visit root_path
+    click_link 'Log In'
   end
 
   test "api account show page holds correct data for github accounts" do
-    visit root_path
     assert page.has_css?("#api_account_#{@api_account.id}"), "Expecting link for api account"
     click_link @api_account.api_username
     assert page.has_content?("Github"), "Page shoud have content GitHub"
-    assert page.has_content?("Username")
-    assert page.has_content?("Languagees")
+    assert page.has_content?("mhartl"), "Page should have content #{@api_account.api_username}"
+    assert page.has_content?("Languages")
     assert page.has_content?("Current Streak")
     assert page.has_content?("Commits This Year")
     assert page.has_content?("Number of Repos")
