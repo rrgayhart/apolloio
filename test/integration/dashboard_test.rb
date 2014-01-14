2c57b4bda5b8e5474d39c5eb82eb929b11454146
QuillyT <QuillyT@gmail.com>
2014-01-13 20:22:44 -0700

    On goal show page click api account name to see api account show page.

--- test/integration/dashboard_test.rb
+++ test/integration/dashboard_test.rb
@@ -4,10 +4,15 @@ class DashboardTest < Capybara::Rails::TestCase
   setup do
     @auth = OmniAuth.config.mock_auth[:twitter]
     @user = User.from_omniauth(@auth)
-    @goal1 = Goal.create(user_id: @user.id, pledge:"Goal1")
-    @goal2 = Goal.create(user_id: @user.id, pledge:"Goal2")
-    @goal3 = Goal.create(user_id: @user.id, pledge:"Goal3")
-    @goal4 = Goal.create(user_id: @user.id+2, pledge:"Goal4")
+
+    @api1 = Api.create(provider: 'github')
+    @api2 = Api.create(provider: 'twitter')
+    @api3 = Api.create(provider: 'exercism')
+
+    @goal1 = Goal.create(user_id: @user.id, pledge:"Goal1", api_account_id: @api1.id)
+    @goal2 = Goal.create(user_id: @user.id, pledge:"Goal2", api_account_id: @api2.id)
+    @goal3 = Goal.create(user_id: @user.id, pledge:"Goal3", api_account_id: @api3.id)
+    @goal4 = Goal.create(user_id: @user.id+2, pledge:"Goal4", api_account_id: @api1.id)
     @goals = [@goal1, @goal2, @goal3]
 
     @reminder1 = Reminder.create(goal_id: @goal1.id, user_id: @user.id, target: 403)
@@ -15,10 +20,6 @@ class DashboardTest < Capybara::Rails::TestCase
     @reminder3 = Reminder.create(goal_id: @goal1.id, user_id: @user.id, target: 1986)
     @reminder4 = Reminder.create(goal_id: @goal1.id + 1, user_id: @user.id + 1, target: 55)
     @reminders = [@reminder1, @reminder2, @reminder3]
-
-    @api1 = Api.create(provider: 'github')
-    @api2 = Api.create(provider: 'twitter')
-    @api3 = Api.create(provider: 'exercism')
 
     @api_account1 = ApiAccount.create(user_id: @user.id, api_id: @api1.id, api_username: "john")
     @api_account2 = ApiAccount.create(user_id: @user.id, api_id: @api2.id, api_username: "steven")
@@ -31,6 +32,10 @@ class DashboardTest < Capybara::Rails::TestCase
     click_on 'Log In'
     assert page.has_content? @goal1.pledge
     click_link @goal1.pledge
+
+    assert page.has_content? @goal1.api_account.api_username
+    click_link @goal1.api_account.api_username
+    assert_equal api_account_path(@goal1.api_account), current_path
   end
 
 end
