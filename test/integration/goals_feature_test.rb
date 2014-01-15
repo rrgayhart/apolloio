f9dba65b5cd4380e357e2d6c6f7684deb1e4666f
Jonah <Jonahkmoses@gmail.com>
2014-01-14 17:26:47 -0700

    WIP

--- test/integration/goals_feature_test.rb
+++ test/integration/goals_feature_test.rb
@@ -34,7 +34,6 @@ class GoalsFeatureTest < Capybara::Rails::TestCase
     assert page.has_content?("Add Reminder")
     click_link "Add Reminder"
     assert page.has_content?("Target")
-
     fill_in "Start date", :with => "01/09/2014"
     fill_in "Target", :with => "2"
     fill_in "Time deadline", :with => "4:00PM"
