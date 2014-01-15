1773f1239633ec80945bb8d3fff1cc4ba801f83e
Meeka <rrgayhart@gmail.com>
2014-01-14 18:09:05 -0700

    Adds provider validations

--- app/models/api_request.rb
+++ app/models/api_request.rb
@@ -3,59 +3,62 @@ require 'json'
 
 class ApiRequest
 
-  attr_reader :username, :user_array
+  attr_reader :username, :user_hash, :provider
 
   def initialize(user, provider, stub=nil)
     @username = user
+    @provider = provider
     if stub
-      @user_array = stub
+      @user_hash = stub
     elsif provider.downcase == "github"
-      @user_array = github_array
+      @user_hash = github_hash
+    else
+      @user_hash = {}
     end
   end
 
-  def github_array
+  def github_hash
     url = "http://github-history.herokuapp.com/find/#{username}.json"
     response = Faraday.get(url)
     JSON.parse(response.body)
   end
 
   def get_streak
-    user_array["current_streak"] || error_message
+    user_hash["current_streak"] || error_message
   end
 
   def get_contributions_today
-    user_array["contributions_today"] || error_message
+    user_hash["contributions_today"] || error_message
   end
 
   def get_days_without_contributions
-    user_array["days_without_contributions"] || error_message
+    user_hash["days_without_contributions"] || error_message
   end
 
   def get_days_this_year_with_contributions
-    user_array["days_this_year_with_contributions"] || error_message
+    user_hash["days_this_year_with_contributions"] || error_message
   end
 
   def get_days_in_the_year
-    user_array["days_in_the_year"] || error_message
+    user_hash["days_in_the_year"] || error_message
   end
 
   def get_percentage_days_commited_this_year
-    user_array["percentage_days_commited_this_year"] || error_message
+    user_hash["percentage_days_commited_this_year"] || error_message
   end
 
   def get_percentage_days_commited_known_history
-    user_array["percentage_days_commited_known_history"] || error_message
+    user_hash["percentage_days_commited_known_history"] || error_message
   end
 
   private
 
   def github_error?
-    user_array["error"]
+    user_hash["error"]
   end
 
   def error_message
-    "#{username} is invalid"
+    "#{username} is not a valid username with #{provider}"
   end
 
 end
