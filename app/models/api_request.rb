5a353ee13353e13a0bfebef61d351fa425bee0cd
Meeka <rrgayhart@gmail.com>
2014-01-14 17:59:59 -0700

    Adds all required methods for github scrape

--- app/models/api_request.rb
+++ app/models/api_request.rb
@@ -21,11 +21,41 @@ class ApiRequest
   end
 
   def get_streak
-    if user_array["error"]
-      user_array["error"]
-    else
-      user_array["current_streak"]
-    end
+    user_array["current_streak"] || error_message
+  end
+
+  def get_contributions_today
+    user_array["contributions_today"] || error_message
+  end
+
+  def get_days_without_contributions
+    user_array["days_without_contributions"] || error_message
+  end
+
+  def get_days_this_year_with_contributions
+    user_array["days_this_year_with_contributions"] || error_message
+  end
+
+  def get_days_in_the_year
+    user_array["days_in_the_year"] || error_message
+  end
+
+  def get_percentage_days_commited_this_year
+    user_array["percentage_days_commited_this_year"] || error_message
+  end
+
+  def get_percentage_days_commited_known_history
+    user_array["percentage_days_commited_known_history"] || error_message
+  end
+
+  private
+
+  def github_error?
+    user_array["error"]
+  end
+
+  def error_message
+    "#{username} is invalid"
   end
 
 end
--- test/models/api_request_test.rb
+++ test/models/api_request_test.rb
@@ -3,18 +3,42 @@ require "test_helper"
 class ApiRequestTest < ActiveSupport::TestCase
 
   def setup
-    @stub = {"username" => "adam89","contributions_today" => 1,"current_streak" => 34,"days_without_contributions" => 248,"days_this_year_with_contributions" => 14, "days_in_the_year" => 15,"percentage_days_commited_this_year" => 0.9333333333333333,"percentage_days_commited_known_history" => 0.3224043715846995}
+    @user
+    @stub = {"username" => "adam89","contributions_today" => 1,"current_streak" => 38,"days_without_contributions" => 248,"days_this_year_with_contributions" => 14, "days_in_the_year" => 15,"percentage_days_commited_this_year" => 0.9333333333333333,"percentage_days_commited_known_history" => 0.3224043715846995}
+    @api_request = ApiRequest.new(@user, "github", @stub)
   end
 
   def test_get_streak
-    user = "adam89"
-    ai = ApiRequest.new(user, "github", @stub)
-    assert_equal 34, ai.get_streak
+    assert_equal 38, @api_request.get_streak
   end
 
   def test_get_error_for_invalid_user
     user = "adamasdfcueahg89"
     ai = ApiRequest.new(user, "github")
-    assert_equal "INVALID USERNAME", ai.get_streak
+    assert_equal "adamasdfcueahg89 is invalid", ai.get_streak
+  end
+
+  def test_get_contributions_today
+    assert_equal 1, @api_request.get_contributions_today
+  end
+
+  def test_get_days_without_contributions
+    assert_equal 248, @api_request.get_days_without_contributions
+  end
+
+  def test_days_this_year_with_contributions
+    assert_equal 14, @api_request.get_days_this_year_with_contributions
+  end
+
+  def test_days_in_the_year
+    assert_equal 15, @api_request.get_days_in_the_year
+  end
+
+  def test_get_percentage_days_commited_this_year
+    assert_equal 0.9333333333333333, @api_request.get_percentage_days_commited_this_year
+  end
+
+  def test_get_percentage_days_commited_known_history
+    assert_equal 0.3224043715846995, @api_request.get_percentage_days_commited_known_history
   end
 end
