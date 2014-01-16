79442ac8eecad1c01ba0b131dde8c2f6de4cedb2
Meeka <rrgayhart@gmail.com>
2014-01-15 21:36:37 -0700

    user without api account is redirected to add api_account

--- test/integration/new_user_signup_test.rb
+++ test/integration/new_user_signup_test.rb
@@ -6,10 +6,17 @@ class NewUserSignupTest < Capybara::Rails::TestCase
     @auth = OmniAuth.config.mock_auth[:twitter]
   end
 
-  test "user_signsup_for_app_and_is_redirected_to_twitter" do
+  test "user_signs_up_for_app_and_is_redirected_to_connect_api_account" do
     visit root_path
     assert_equal current_path, root_path
+    
+    @auth = OmniAuth.config.mock_auth[:twitter]
+    user = User.from_omniauth(@auth)
+    api1  = FactoryGirl.create(:api, :github)
+    api2  = FactoryGirl.create(:api, :fitbit)
+    api3  = FactoryGirl.create(:api, :exercism)
     click_link "Log In"
+    assert page.has_content?("Connect An API Account")
   end
 
 end
