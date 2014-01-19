da37b671145b2fd1491a5f78977e7d5e25b933f6
Adam89 <Adamm1@aston.ac.uk>
2014-01-18 17:27:53 -0700

    working on basic stats

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,17 +19,28 @@ class UserTest < ActiveSupport::TestCase
       u.provider = "twitter"
     end
   end
-   
+
   def test_from_omniauth_finds_user_that_exists
     user = User.from_omniauth(omniauth_hash)
     assert_equal user, User.from_omniauth(omniauth_hash)
   end
 
-  def test_user_has_api_account_method_returns_true
-    user = FactoryGirl.create(:user)
-    assert_equal false, user.has_api_account?
-    FactoryGirl.create(:api_account, user: user)
-    assert_equal true, user.has_api_account?
+  def test_a_user_has_no_api_accounts_if_its_api_accounts_are_empty
+    user = User.new(api_accounts: [])
+
+    assert(!user.has_api_account?)
   end
+
+  def test_a_user_has_api_accounts_if_its_api_accounts_are_not_empty
+    user = User.new(api_accounts: ['anything'])
+
+    assert(user.has_api_account?)
+  end
+
+
+  #testing active record
+  #testing the behaviour of associations
+  #how an article determines what its user is
+  #use mocks and stubs
 
 end
