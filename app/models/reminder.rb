482266c2f1999022a6c9445d026953fe6e370303
Adam89 <Adamm1@aston.ac.uk>
2014-01-20 18:58:46 -0700

    removed two methods from reminder

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -12,22 +12,22 @@ class Reminder < ActiveRecord::Base
   end
 
   def render_day_name(day_number)
-    Date::DAYNAMES[day_number]
+    #Date::DAYNAMES[day_number]
   end
 
   def render_month_name(month_number)
-    Date::MONTHNAMES[month_number]
+    #Date::MONTHNAMES[month_number]
   end
 
   def goal_render
     number = self.day_deadline || 0
     case self.goal.period_type
     when "weeks", "weekly"
-      "Every #{render_day_name(number)}"
+     "Every #{render_day_name(number)}"
     when "months", "monthly"
       "#{number.ordinalize} of the month"
     when "day", "daily", "days"
-      "Daily"
+     "Daily"
     when "yearly", "years"
       render_month_name(number)
     else
