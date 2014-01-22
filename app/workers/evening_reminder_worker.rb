7a0e0ac0c36a20867b13f3954e8747c786fa8b5e
Jonah <Jonahkmoses@gmail.com>
2014-01-21 22:26:04 -0700

    adjust background workers for correct times

--- app/workers/evening_reminder_worker.rb
+++ app/workers/evening_reminder_worker.rb
@@ -2,16 +2,13 @@ class EveningReminderWorker
   include Sidekiq::Worker
 
   def perform
-    logger.info "got here"
     reminders = Reminder.where(time_deadline: "Evening At 19:00", active: true)
     reminders.each { |reminder| send_notification(reminder) }
   end
 
   def send_notification(reminder)
     user = reminder.user
-    # send email if notification.email?
     EmailNotifications.reminder_confirmation(user).deliver if reminder.email
-    # send sms if notification.sms?
     SmsNotifications.send_sms(user, reminder) if reminder.sms
   end
   
--- app/workers/morning_reminder_worker.rb
+++ app/workers/morning_reminder_worker.rb
@@ -1,18 +1,15 @@
-# class MorningReminderWorker 
-#   include Sidekiq::Worker
+class MorningReminderWorker 
+  include Sidekiq::Worker
 
-#   def perform
-#     # find all reminders that are at morning
-#     reminders = Reminder.where(time_deadline: "Morning", active: true)
-#     reminders.each { |reminder| send_notification(reminder) }
-#   end
+  def perform
+    reminders = Reminder.where(time_deadline: "Morning At 9:00", active: true)
+    reminders.each { |reminder| send_notification(reminder) }
+  end
 
-#   def send_notification(reminder)
-#     user = reminder.user
-#     # send email if notification.email?
-#     EmailNotifications.reminder_confirmation(user).deliver if reminder.email
-#     # send sms if notification.sms?
-#     SmsNotifications.send_sms(user, reminder) if reminder.sms
-#   end
+  def send_notification(reminder)
+    user = reminder.user
+    EmailNotifications.reminder_confirmation(user).deliver if reminder.email
+    SmsNotifications.send_sms(user, reminder) if reminder.sms
+  end
   
-# end
+end
